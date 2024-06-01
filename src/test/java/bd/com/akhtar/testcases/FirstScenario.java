9700f756d914249e6e09f21ee54de6fa7a4fb080
Aurnob <aurnob3c@gmail.com>
2024-06-02 00:39:04 +0600

    testcase

--- src/test/java/bd/com/akhtar/testcases/FirstScenario.java
+++ src/test/java/bd/com/akhtar/testcases/FirstScenario.java
@@ -16,24 +16,21 @@ public class FirstScenario extends Base {
 		System.out.println("Starting...");
 		driver = initializeBrowserAndOpenApplicationURL("chrome");
 		guestUser = new GuestUser(driver);
-
 	}
 
-	@Test(description = "Scenario: Add products to the shopping cart\n" + "Given: User is on the home page\n"
-			+ "When: User hovers over 'Living'\n" + "And: User clicks 'Sofa'\n" + "And: User clicks 'Conrad Sofa'\n"
-			+ "And: User scrolls down to 'Add to Cart'\n" + "And: User clicks 'PREDICTED DELIVERY TIME'\n"
-			+ "And: User selects 'Dhaka' as the location\n" + "And: User selects 'Uttara' as the nearest showroom\n"
-			+ "And: User selects 'PU Rexine' as the material\n" + "And: User selects 'Standard Size'\n"
-			+ "And: User sets the quantity to '2'\n" + "Then: User clicks 'Add to Cart'\n"
-			+ "And: User clicks 'View Cart'")
+	@Test(description = "Scenario: Add products to the shopping cart\n" +
+			"Given: User is on the home page\n" +
+			"When: User hovers over 'Living', clicks 'Sofa', clicks 'Conrad Sofa'\n" +
+			"And: User scrolls down to 'Add to Cart' and clicks 'PREDICTED DELIVERY TIME'\n" +
+			"And: User selects their location\n" +
+			"And: User fills in data about product details\n" +
+			"Then: User clicks 'Add to Cart'\n")
 	public void testAddProductsToCart() throws InterruptedException {
 		guestUser.FirstExecution();
 	}
 
 	@AfterMethod
 	public void tearDownTest() {
-		{
-			driver.quit();
-		}
+		driver.quit();
 	}
 }
